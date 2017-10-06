35aaba3c96c2b89eaea50227f77c40c0408af99a
ponychan <rzsckbmx@163.com>
2017-10-06 14:07:45 +0800

    4.7.cpp
    用结构体储存学生信息

--- ConsoleApplication4_7/ConsoleApplication47.cpp
+++ ConsoleApplication4_7/ConsoleApplication47.cpp
@@ -1,11 +1,27 @@
 // ConsoleApplication47.cpp: 定义控制台应用程序的入口点。
-//
+//4.7.cpp用结构体储存学生信息
 
 #include "stdafx.h"
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
 
+//定义结构体储存学生信息
+struct student {
+	int num;
+	string name;
+	char sex;
+	int age;
+};
 
 int main()
 {
-    return 0;
+	student stu = { 97001,"LinLin",'F',19 };
+	cout << "num : " << stu.num << endl;
+	cout << "name: " << stu.name << endl;
+	cout << "sex : " << stu.sex << endl;
+	cout << "age : " << stu.age << endl;
+	return 0;
 }
 
